06d90c031f4d44bba4347c2a16d16b6b3f5036dd
tina-e <tina.emmert@web.de>
2023-05-05 15:19:28 +0200

    rename file to match the requirements pdf

--- karaoke-game/Menu.py
+++ karaoke-game/Menu.py
@@ -8,6 +8,7 @@ class Menu:
         self.welcome = text.Label('WELCOME!', font_size=40, x=win_w / 2, y=win_h / 1.8, anchor_x='center', anchor_y='center')
         self.winner = text.Label('YOU ROCK!', font_size=40, x=win_w / 2, y=win_h / 1.8, anchor_x='center', anchor_y='center')
         self.loser = text.Label('YOU SUCK!', font_size=40, x=win_w / 2, y=win_h / 1.8, anchor_x='center', anchor_y='center')
+        self.instructions_control = text.Label('Whistling recommended.', font_size=16, x=win_w / 2, y=win_h / 3, anchor_x='center', anchor_y='center')
         self.instructions_start = text.Label('Start: SPACE', x=win_w / 2, y=win_h / 4, anchor_x='center', anchor_y='center')
         self.instructions_exit = text.Label('Exit: Q', x=win_w / 2, y=win_h / 5, anchor_x='center', anchor_y='center')
 
@@ -15,6 +16,7 @@ class Menu:
         self.background.draw()
         if welcome:
             self.welcome.draw()
+            self.instructions_control.draw()
         else:
             if score >= self.win_score:
                 self.winner.draw()
