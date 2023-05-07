fa37e24710e4cda86966778532cacea55e3c8758
tina-e <tina.emmert@web.de>
2023-05-07 18:31:56 +0200

    fix too sensitive whistling detection

--- whistle-input/StreamAnalyzer.py
+++ whistle-input/StreamAnalyzer.py
@@ -19,7 +19,7 @@ class StreamAnalyzer:
         :return: (UP, DOWN or NONE) detected frequency change
         '''
         current_freq = self.calculate_frequency(data)
-        # filter silence / noise / non-typical whistling frequencies
+        # filter silence / non-typical whistling frequencies
         if current_freq <= 15 or current_freq > 100:
             self.silence += 1
             self.registration = []
@@ -33,9 +33,6 @@ class StreamAnalyzer:
             # if frequency lower than before
             elif current_freq < self.frequencies[-1]:
                 self.registration.append('l')
-            # if frequency same as before
-            else:
-                self.registration.append('s')
 
         self.frequencies.append(current_freq)
         return self.handle_data_change()
@@ -43,25 +40,24 @@ class StreamAnalyzer:
     def handle_data_change(self):
         '''
         analyze registered frequency change patterns
-        if multiple registrations stored, the most occurring value it is
+        as soon as there is an isolated sound with changing frequency
         :return: (UP, DOWN or NONE) detected frequency change
         '''
         len_registration = len(self.registration)
-        if self.silence >= 5:
+        if self.silence >= 10 and len_registration >= 3:
             self.silence = 0
-            print(self.registration)
             occurrence_lower = self.registration.count('l')
             occurrence_higher = self.registration.count('h')
 
-            # detect increasing frequency
+            # detect decreasing frequency (most of the frequency changes were 'lower f than before')
             if occurrence_lower >= (0.6 * len_registration):
                 self.frequencies = []
                 self.registration = []
-                return 'UP'
-            # detect decreasing frequency
+                return 'DOWN'
+            # detect increasing frequency (most of the frequency changes were 'higher f than before')
             elif occurrence_higher >= (0.6 * len_registration):
                 self.frequencies = []
                 self.registration = []
-                return 'DOWN'
+                return 'UP'
         return 'NONE'
 
